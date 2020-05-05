2109d30fceb3fe1bf40048f1a444a55af689c1c1
Albert Cervantes <cervanalbert@gmail.com>
2020-05-04 19:34:40 -0700

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,68 @@
 // when the app is used offline. HINT: You should use two caches. One for the
 // static assets such ass html, css, js, images, etc; and another cache for
 // the dynamic data from requests to routes beginning with "/api".
+const FILES_TO_CACHE = [
+    "/",
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/manifest.json",
+    "/style.css"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  }
+);
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+                cache.put(event.request.url, response.clone());
+                return response;
+            })
+            .catch(() => {
+              return cache.match(event.request);
+            });
+        })
+        .catch(err => console.log(err))
+      );
+      return;
+    }
+  
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+        });
+      })
+    );
+});
